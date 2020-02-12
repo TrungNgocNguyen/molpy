42fe0ddeecc73a9c14db9489987c8f6d96f1f79c
Trung Ngoc Nguyen <tn.nguyen@fu-berlin.de>
2020-02-12 13:58:31 +0100

    Parametrized test_distance, created test_distance_failure

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -1,7 +1,19 @@
 
+import pytest
 import molpy
 
 
-def test_distance():
+@pytest.mark.parametrize("point1, point2, bench", [
+    ([0, 1], [0, 0], 1),
+    ([0, 0], [0, 0], 0),
+    ([-1, -1, -4], [-1, -1, 4], 8),
+    ([2.236, 2.236], [0, 0], 3.162)
+])
+def test_distance(point1, point2, bench):
 
-    assert molpy.util.distance([0, 1], [0, 0]) == 1
+    assert molpy.util.distance(point1, point2) == pytest.approx(bench, abs=1.e-3)
+
+
+def test_distance_failure():
+
+    assert molpy.util.distance([0], [3]) != 5
